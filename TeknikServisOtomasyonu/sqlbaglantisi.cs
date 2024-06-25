8443ddc3e53d9ac3c5bc749897a501d49c1040cd
Enes Karakaya <Enes.krkya1453@gmail.com>
2024-06-25 11:37:41 +0300

    a

---
+++ TeknikServisOtomasyonu/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TeknikServisOtomasyonu
+{
+    class sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-ILG2BKJ\SQLEXPRESS;Initial Catalog=TeknikServis;Integrated Security=True;Encrypt=False");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
